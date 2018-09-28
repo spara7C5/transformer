ec1801d3fc180ff1e40a7a31302c804804e1a459
spara7C5 <paracchino@hotmail.it>
2018-09-28 11:11:02 +0200

    reorganization of the reading section

--- FFT.py
+++ FFT.py
@@ -15,34 +15,50 @@ filtering=0
 mirroring=0
 
 ######## opt 1) reading data #################
-#tvec,Svec=genfromtxt("../data",delimiter='\t',usecols=(0,1),max_rows=1000000,unpack=True)
-#Ts=(tvec[11]-tvec[10])
-#lo=cos(2*pi*(10**6)*tvec)
-#S=Svec*lo
+#(((((((((((((((((((((((((((((((((
+######## 1_1) optical hybrid #################
+tvec,rex_r,imx_r,imy_r,rey_r=genfromtxt("../data",delimiter='\t',max_rows=1000000,unpack='True')
+lo=cos(2*pi*(10**6)*tvec)
+rex,imx,rey,imy=rex_r*lo,imx_r*lo,rey_r*lo,imy_r*lo
+#Svec=[rey[x]/sqrt(rex[x]**2 + imx[x]**2 + rey[x]**2 + imy[x]**2) for x in range(len(tvec))]
+
+######## 1_2) single data channel ############
+
+#tvec,Svec=genfromtxt("../GIT/#rex.csv",delimiter='\t',max_rows=1000000,unpack='True')
+
 ##############################################
 
-myrand=RandomWalk()
+Ts=(tvec[11]-tvec[10])
+S=rex
+plot(tvec,S)
+#)))))))))))))))))))))))))))))))))
+##############################################
+
 ######## opt 2) generating noise #############
-Ts=1*10**(-3) 
-Fs=1/Ts
-psd_noise= 7 # [V^2/Hz]
-amp=sqrt(psd_noise*(Fs/2)) # [V]
+#(((((((((((((((((((((((((((((((((((((
+#myrand=RandomWalk()
+#Ts=1*10**(-3) 
+#Fs=1/Ts
+#psd_noise= 7 # [V^2/Hz]
+#amp=sqrt(psd_noise*(Fs/2)) # [V]
+
 ######### 2_1) white noise##############
 #Svec=random.normal(0,sqrt(7*500),10000)
 ########################################
 
 ########## 2_2 rand walk ##############
-amprand=sqrt((10**6)*2*pi*pi/Fs)
-Svec=[(amprand)*myrand.funrand(1) for i in range(100000) ]
+#amprand=sqrt((10**3)*2*pi*pi/Fs)
+#Svec=[2+1*cos(2*pi*40*Ts*i)+(amprand)*myrand.funrand(i) for i in range(100000) ]
 #######################################
 
-tvec=array([Ts*i for i in range(100000)])
+#tvec=array([Ts*i for i in range(100000)])
 #fig0=figure();plot(tvec,Svec);title("rand noise")
-S=Svec
+#S=Svec
+#))))))))))))))))))))))))))))))))))))))
 #######################################
 
 Fs=1/Ts
-N=len(Svec)
+N=len(S)
 
 
 if mirroring:
@@ -82,7 +98,8 @@ xax.set_scientific(True)
 ax2.grid(linestyle="--")
 #####IMPORTANT: f=0 must be omitted in PSD ##########
 print(mean(Ysmod[1:int(N/2)]))
-ax2.semilogy()
+#ax2.semilogy()
+ax2.loglog()
 
 
 fpsd,Ppsd=signal.welch(S,fs=Fs,window='boxcar',scaling='density',nperseg=len(S))
